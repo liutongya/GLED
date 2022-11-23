7bc12e48a821640409152b1313ecb144e3cd94ad
liutongya <liutongya@sio.org.cn>
2022-11-23 13:11:31 +0800

    update

--- README.md
+++ README.md
@@ -1,15 +1,33 @@
 # A global Lagrangian eddy dataset based on satellite altimetry (GLED v1.0)
+Tongya Liu and Ryan Abernathey
 
 [![DOI](https://zenodo.org/badge/182862122.svg)](https://zenodo.org/badge/latestdoi/182862122)
 [![Zenodo:Data](https://img.shields.io/badge/Zenodo:Data-10.5281/zenodo.2648855-blue.svg)](https://zenodo.org/record/2648855)
 
 ![Image text](scripts/fig_lavd.png)
 
-Code repository to reproduce the generation of GLED v1.0. 
-[Busecke_etal 2019, published in GRL](https://agupubs.onlinelibrary.wiley.com/doi/full/10.1029/2019GL082692)
 
 ## 1 Introduction
 
+Code repository to reproduce the generation of GLED v1.0. 
+
 ## 2 Dataset description
+This open-source dataset contains not only the general features (eddy center position, equivalent radius, rotation property, etc.) of eddies with lifetimes of 30, 90, and 180 days but also the trajectories of particles trapped by coherent eddies over the lifetime.
+
+First, the general features of coherent eddies are provided in the file named eddy_info. 
+- id: the eddy's ID
+- date_start: generation date of the eddy
+- duration: eddy lifespan (in days)
+- radius: eddy radius (in km)
+- cyc: 1 for anticyclonic and -1 for cyclonic
+- center_lon, center_lat: the longitude and latitude of the eddy center with a frequency of 10 days
+- dx, dy: zonal and meridional displacements (in km) over the eddy lifetime
+- speed_x, speed_y: the zonal and meridional propagation speeds (in m/s) of the eddy
+- lavd: values of Lagrangian-averaged vorticity deviation (LAVD)
+- cd, ci: values of convexity deficiency (CD) and coherency index (CI)
+
+Second, the trajectories of all Lagrangian particles inside the eddy boundary are provided in the file named eddy_traj. The particle positions every 10 days for each eddy are stored in an NC file with a three-dimensional array.
+
 
 ## 3 Data visualization
+See the notebooks in *scripts*.
---
+++ scripts/01.run_vel_corr.py
@@ -0,0 +1,112 @@
+# Velocity correction to remove the divergence following Abernathey and Marshall (2013)
+
+import xarray as xr
+import numpy as np
+from xmitgcm import open_mdsdataset
+from xgcm import Grid
+from xgcm.autogenerate import generate_grid_ds
+
+
+dimXList = ['lon', 'longitude', 'LON', 'LONGITUDE', 'geolon', 'GEOLON',
+            'xt_ocean']
+dimYList = ['lat', 'latitude' , 'LAT', 'LATITUDE' , 'geolat', 'GEOLAT',
+            'yt_ocean']
+
+def write_field(fname, data):
+    print('wrote to file: ' + fname)
+    fid = open(fname, "wb")
+    data.tofile(fid)
+    fid.close()
+
+def add_MITgcm_missing_metrics(dset, periodic=None, boundary=None):
+    """
+    Infer missing metrics from MITgcm output files.
+
+    Parameters
+    ----------
+    dset : xarray.Dataset
+        A dataset open from a file
+    periodic : str
+        Which coordinate is periodic
+    boundary : dict
+        Default boundary conditions applied to each coordinate
+
+    Return
+    -------
+    dset : xarray.Dataset
+        Input dataset with appropriated metrics added
+    grid : xgcm.Grid
+        The grid with appropriated metrics
+    """
+    coords = dset.coords
+    grid   = Grid(dset, periodic=periodic, boundary=boundary)
+    
+    if 'drW' not in coords: # vertical cell size at u point
+        coords['drW'] = dset.hFacW * dset.drF
+    if 'drS' not in coords: # vertical cell size at v point
+        coords['drS'] = dset.hFacS * dset.drF
+    if 'drC' not in coords: # vertical cell size at tracer point
+        coords['drC'] = dset.hFacC * dset.drF
+    if 'drG' not in coords: # vertical cell size at tracer point
+        coords['drG'] = dset.Zl - dset.Zl + dset.drC.values[:-1]
+        # coords['drG'] = xr.DataArray(dset.drC[:-1].values, dims='Zl',
+        #                              coords={'Zl':dset.Zl.values})
+    
+    if 'dxF' not in coords:
+        coords['dxF'] = grid.interp(dset.dxC, 'X')
+    if 'dyF' not in coords:
+        coords['dyF'] = grid.interp(dset.dyC, 'Y')
+    if 'dxV' not in coords:
+        coords['dxV'] = grid.interp(dset.dxG, 'X')
+    if 'dyU' not in coords:
+        coords['dyU'] = grid.interp(dset.dyG, 'Y')
+    
+    if 'hFacZ' not in coords:
+        coords['hFacZ'] = grid.interp(dset.hFacS, 'X')
+    if 'maskZ' not in coords:
+        coords['maskZ'] = coords['hFacZ']
+        
+    if 'yA' not in coords:
+        coords['yA'] = dset.drF * dset.hFacC * dset.dxF
+    
+    metrics = {
+        ('X',)    : ['dxG', 'dxF', 'dxC', 'dxV'], # X distances
+        ('Y',)    : ['dyG', 'dyF', 'dyC', 'dyU'], # Y distances
+        ('Z',)    : ['drW', 'drS', 'drC', 'drF', 'drG'], # Z distances
+        ('X', 'Y'): ['rAw', 'rAs', 'rA' , 'rAz'], # Areas in X-Y plane
+        ('X', 'Z'): ['yA']} # Areas in X-Z plane
+    
+    grid._assign_metrics(metrics)
+    
+    return dset, grid
+
+
+path = '/data/home/liutongya/RCLV/velocity/run_offline/run/'
+ds = open_mdsdataset(path, prefix={'Diag_stat'}).chunk()
+
+grid = Grid(ds)
+
+ds, grid = add_MITgcm_missing_metrics(ds, periodic='X', boundary={'Y':'extend'})
+
+# path to save the corr velocity
+path_corr = '/data/home/liutongya/RCLV/velocity/vel_corr/'
+
+#for num in np.arange(9861):
+for num in np.arange(1163,1171,1):
+    phi = ds.PhiVEL[num, :, :]
+
+    u_phi = xr.where(ds.maskW, grid.derivative(phi, 'X'), 0)
+    v_phi = xr.where(ds.maskS, grid.derivative(phi, 'Y'), 0)
+
+    u_nd = (ds.UVEL[num, :, :] - u_phi).values.astype('>f4')
+    v_nd = (ds.VVEL[num, :, :] - v_phi).values.astype('>f4')
+    
+    u_out = path_corr + 'uvel.' + str(num).zfill(10) + '.data'
+    v_out = path_corr + 'vvel.' + str(num).zfill(10) + '.data'
+    
+    write_field(u_out, u_nd)
+    write_field(v_out, v_nd)
+    
+    
+
+
